cc5b46f457720fbdc1d03599f78b9db55a478b28
Clementine <clem.duc@proton.me>
2026-02-12 19:00:01 +0100

    add condition to start a cycle

--- src/CycleManager/CycleManager.gd
+++ src/CycleManager/CycleManager.gd
@@ -5,23 +5,18 @@ extends Control
 @onready var long_input = %InputLongue
 @onready var work_input = %InputTravail
 
-
-
 func _on_ButtonValider_pressed():
+	if [
+		cycle_input.text,
+		short_input.text,
+		long_input.text,
+		work_input.text
+	].any(func(el: String): return el == ""):
+		return
 	var session_config = {
 		cycle_number = int(cycle_input.text), 
 		short_time = int(short_input.text) * 60, 
 		long_time = int(long_input.text) * 60, 
 		work_time = int(work_input.text) * 60
-	}
-	E.emit_signal("session_started", session_config)
-
-
-func _on_ButtonTest_pressed():
-	var session_config = {
-		cycle_number = 3, 
-		short_time = 10, 
-		long_time = 20, 
-		work_time = 30
 	}
 	E.emit_signal("session_started", session_config)
